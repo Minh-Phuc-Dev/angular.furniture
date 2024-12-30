c472f5324fb5fa70360f1b50977b0a42b9ab6763
Minh Trung <nguyenminhtrungse@gmail.com>
2024-12-30 07:58:22 +0700

    update source

--- Frontend/src/helpers/index.ts
+++ Frontend/src/helpers/index.ts
@@ -1,3 +1,7 @@
 export * from "helpers/Request"
 
-export const getImageUrl = (url: string | undefined) => `http://localhost:8080/api/media/${url}`
+export function getImageUrl(image?: string) {
+    if (!image) return "/images/no-image.jpg"
+
+    return `http://localhost:8080${image}`
+}
