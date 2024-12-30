734b5ffe340078381d1ba03c88a6f29c7e707099
Minh Trung <nguyenminhtrungse@gmail.com>
2024-12-30 07:59:16 +0700

    fix url image

--- Frontend/src/helpers/index.ts
+++ Frontend/src/helpers/index.ts
@@ -3,5 +3,5 @@ export * from "helpers/Request"
 export function getImageUrl(image?: string) {
     if (!image) return "/images/no-image.jpg"
 
-    return `http://localhost:8080${image}`
+    return `http://localhost:8080/api/media/${image}`
 }
